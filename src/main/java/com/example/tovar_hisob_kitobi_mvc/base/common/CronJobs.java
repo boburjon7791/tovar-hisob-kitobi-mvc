28c7cabe04409434af3f750cd5fc3f288adfee12
Boburjon <soliyevboburjon95@gmail.com>
2024-12-15 20:08:18 +0500

    added get database backup and old database backup delete functionality

--- src/main/java/com/example/tovar_hisob_kitobi_mvc/base/common/CronJobs.java
+++ src/main/java/com/example/tovar_hisob_kitobi_mvc/base/common/CronJobs.java
@@ -1,31 +1,27 @@
 package com.example.tovar_hisob_kitobi_mvc.base.common;
 
-import com.example.tovar_hisob_kitobi_mvc.base.model.entity.BaseEntity;
-import com.example.tovar_hisob_kitobi_mvc.implementation.prixod.model.entity.PrixodTovar;
 import com.example.tovar_hisob_kitobi_mvc.implementation.prixod.repository.PrixodTovarDetailRepository;
 import com.example.tovar_hisob_kitobi_mvc.implementation.prixod.repository.PrixodTovarRepository;
-import com.example.tovar_hisob_kitobi_mvc.implementation.rasxod.model.entity.RasxodTovar;
 import com.example.tovar_hisob_kitobi_mvc.implementation.rasxod.repository.RasxodTovarDetailRepository;
 import com.example.tovar_hisob_kitobi_mvc.implementation.rasxod.repository.RasxodTovarRepository;
-import com.example.tovar_hisob_kitobi_mvc.implementation.vozvrat.model.entity.VozvratTovar;
 import com.example.tovar_hisob_kitobi_mvc.implementation.vozvrat.repository.VozvratTovarDetailRepository;
 import com.example.tovar_hisob_kitobi_mvc.implementation.vozvrat.repository.VozvratTovarRepository;
-import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.nio.file.Path;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
+@Slf4j
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
@@ -37,6 +33,17 @@ public class CronJobs {
     private final VozvratTovarRepository vozvratTovarRepository;
     private final VozvratTovarDetailRepository vozvratTovarDetailRepository;
     private static final String loggingFolder="logging_folder";
+
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+
+    private static final String BACKUP_FOLDER="db-backup-files";
 
     @Scheduled(cron = "0 * * * * *")
     @Transactional
@@ -63,6 +70,51 @@ public class CronJobs {
         for (File file : Objects.requireNonNullElse(new File(loggingFolder).listFiles(),new File[]{})) {
             if (file.getName().contains(localDate)) {
                 System.out.println("file.delete() = " + file.delete());
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void backupDatabase(){
+        try {
+            File backupFilesFolder = new File(BACKUP_FOLDER);
+            if (!backupFilesFolder.exists()) {
+                System.out.println("backupFilesFolder.mkdirs() = " + backupFilesFolder.mkdirs());
+            }
+            String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            String databaseName = databaseUrl.substring(databaseUrl.lastIndexOf("/") + 1);
+            String backupFile = BACKUP_FOLDER + "/" + databaseName + "-" + date + ".dump";
+
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "pg_dump", "-U", databaseUsername, "-F", "c", "-d", databaseName, "-f", backupFile);
+            processBuilder.environment().put("PGPASSWORD",databasePassword);
+            System.out.println("databaseName = " + databaseName);
+            System.out.println("databaseUsername = " + databaseUsername);
+            System.out.println("databasePassword = " + databasePassword);
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            process.waitFor();
+
+            if (process.exitValue()==0) {
+                log.info("db backup was written : {}",backupFile);
+                cleanOldBackups(backupFilesFolder);
+            }else {
+                log.error("error while writing db backup : {}",backupFile);
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void cleanOldBackups(File backupFolder){
+        long currentTime = System.currentTimeMillis();
+        int day=1;
+        for (File file : backupFolder.listFiles()) {
+            if (file.getName().endsWith(".dump")) {
+                long fileAge = currentTime-file.lastModified();
+                if (fileAge>day*24*60*60*1000L && file.delete()) {
+                    log.info("deleted old db backup : {}",file.getName());
+                }
             }
         }
     }
